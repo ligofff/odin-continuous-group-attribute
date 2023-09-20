9f88b12bd4ebbb5944cb95d82c5d0c491ed4064f
ligofff <liga@ligofff.ru>
2023-09-20 17:23:42 +0300

    Main package init

---
+++ Editor/ContinuousTabGroupAttributeProcessor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+
+namespace OdinContinuousGroup
+{
+    public class BeginAndEndGroupAttributeProcessor<T> : OdinAttributeProcessor<T>
+    {
+        private ContinuousGroupAttribute _groupStartsFrom;
+        
+        public override void ProcessChildMemberAttributes(InspectorProperty parentProperty, MemberInfo member, List<Attribute> attributes)
+        {
+            if (attributes.OfType<ContinuousGroupAttribute>().FirstOrDefault() is {} startAttribute)
+            {
+                _groupStartsFrom = startAttribute;
+            }
+            
+            if (attributes.OfType<ContinuousGroupEndAttribute>().FirstOrDefault() is {} endAttribute)
+            {
+                _groupStartsFrom = null;
+            }
+
+            if (_groupStartsFrom is { })
+            {
+                PropertyGroupAttribute propertyGroupAttribute;
+
+                switch (_groupStartsFrom.groupType)
+                {
+                    case GroupType.TabGroup:
+                        propertyGroupAttribute = new TabGroupAttribute(_groupStartsFrom.tabName);
+                        break;
+                    case GroupType.BoxGroup:
+                        propertyGroupAttribute = new BoxGroupAttribute(_groupStartsFrom.tabName);
+                        break;
+                    case GroupType.TitleGroup:
+                        propertyGroupAttribute = new TitleGroupAttribute(_groupStartsFrom.tabName);
+                        break;
+                    case GroupType.FoldoutGroup:
+                        propertyGroupAttribute = new FoldoutGroupAttribute(_groupStartsFrom.tabName);
+                        break;
+                    case GroupType.HorizontalGroup:
+                        propertyGroupAttribute = new HorizontalGroupAttribute(_groupStartsFrom.tabName);
+                        break;
+                    case GroupType.VerticalGroup:
+                        propertyGroupAttribute = new VerticalGroupAttribute(_groupStartsFrom.tabName);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                
+                attributes.Add(propertyGroupAttribute);
+            }
+        }
+    }
+}
---
+++ Runtime/ContinuousTabGroup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OdinContinuousGroup
+{
+    public class ContinuousGroupAttribute : Attribute
+    {
+        public string tabName;
+        public GroupType groupType;
+
+        public ContinuousGroupAttribute(string tabName, GroupType groupType = GroupType.BoxGroup)
+        {
+            this.tabName = tabName;
+            this.groupType = groupType;
+        }
+    }
+
+    public enum GroupType
+    {
+        TabGroup,
+        BoxGroup,
+        TitleGroup,
+        FoldoutGroup,
+        HorizontalGroup,
+        VerticalGroup
+    }
+    
+    public class ContinuousGroupEndAttribute : Attribute
+    {
+        
+    }
+}
